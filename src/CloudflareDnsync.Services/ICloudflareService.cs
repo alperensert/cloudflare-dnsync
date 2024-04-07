2853b3e12a4ca560929f13104b200575f00fdeb2
alperensert <63921520+alperensert@users.noreply.github.com>
2024-04-08 01:52:01 +0300

    refactor: cloudflare and configuration service interfaces

--- src/CloudflareDnsync.Services/ICloudflareService.cs
+++ src/CloudflareDnsync.Services/ICloudflareService.cs
@@ -1,6 +1,11 @@
-﻿namespace CloudflareDnsync.Services;
+﻿using CloudflareDnsync.Models;
+using CloudflareDnsync.Models.Responses;
+
+namespace CloudflareDnsync.Services;
 
 public interface ICloudflareService
 {
+    Task<CloudflareResponse<List<Zone>>> GetZonesAsync(int page = 1, int perPage = 20);
 
+    Task<CloudflareResponse<TokenVerify>> VerifyTokenAsync();
 }
--- src/CloudflareDnsync.Services/IDnsyncConfigService.cs
+++ src/CloudflareDnsync.Services/IDnsyncConfigService.cs
@@ -1,6 +1,18 @@
-﻿namespace CloudflareDnsync.Services;
+﻿using CloudflareDnsync.Models;
+
+namespace CloudflareDnsync.Services;
 
 public interface IDnsyncConfigService
 {
+    IEnumerable<DnsyncConfiguration> GetAll();
 
+    DnsyncConfiguration? GetById(string id);
+
+    DnsyncConfiguration? GetByName(string name);
+
+    Task AddAsync(DnsyncConfiguration configuration, CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(DnsyncConfiguration configuration, CancellationToken cancellationToken = default);
+
+    Task RemoveByNameAsync(string name, CancellationToken cancellationToken = default);
 }
