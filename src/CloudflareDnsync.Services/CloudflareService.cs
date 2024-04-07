2cd0fe5176d4b95f7f4e6bfd3d9f78572921f4d2
alperensert <63921520+alperensert@users.noreply.github.com>
2024-04-07 21:43:41 +0300

    Initial commit

---
+++ src/CloudflareDnsync.Services/CloudflareService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using CloudflareDnsync.Abstractions;
+using Newtonsoft.Json;
+
+namespace CloudflareDnsync.Services;
+
+public class CloudflareService : ICloudflareService
+{
+    private const string BaseUrl = "https://api.cloudflare.com/client/v4/";
+
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri(BaseUrl)
+    };
+
+    public CloudflareService(string apiKey, string apiEmail, string? baseAddress = null)
+    {
+        _httpClient.DefaultRequestHeaders.Add("X-Auth-Key", apiKey);
+        _httpClient.DefaultRequestHeaders.Add("X-Auth-Email", apiEmail);
+        if (baseAddress is not null)
+            _httpClient.BaseAddress = new Uri(baseAddress);
+    }
+
+    public CloudflareService(string token, string? baseAddress = null)
+    {
+        _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+        if (baseAddress is not null)
+            _httpClient.BaseAddress = new Uri(baseAddress);
+    }
+
+    private async Task<TResult> SendAsync<TResult>(HttpMethod method, string url, object? data = null)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        if (data is not null && (method == HttpMethod.Patch || method == HttpMethod.Post || method == HttpMethod.Put))
+        {
+            var json = JsonConvert.SerializeObject(data);
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+        }
+        var response = await _httpClient.SendAsync(request);
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<TResult>(content)!;
+    }
+}
