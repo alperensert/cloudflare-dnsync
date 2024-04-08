126d65d359d3863a12715790d9870ccb31cec2ad
alperensert <63921520+alperensert@users.noreply.github.com>
2024-04-08 22:16:20 +0300

    feat: refactor cli project to add required package references and infrastructure classes
    Add logging, dependency injection and cli packages to CLI project. Create type registrar and type resolver for supports dependency injection in Spectre.Console.Cli

--- src/CloudflareDnsync.Cli/Program.cs
+++ src/CloudflareDnsync.Cli/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+using CloudflareDnsync.Cli.Infrastructure;
+using CloudflareDnsync.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddSingleton<IDnsyncConfigService, DnsyncConfigService>();
+
+var registrar = new TypeRegistrar(serviceCollection);
+
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
+{
+    config.SetApplicationName("cloudflare-dnsync");
+    config.SetApplicationVersion("1.0.0");
+});
+
+return await app.RunAsync(args);
