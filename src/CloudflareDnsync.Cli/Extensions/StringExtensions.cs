6d355bc2fd89e5a28f81799ad048177a2ad055fb
alperensert <63921520+alperensert@users.noreply.github.com>
2024-04-09 00:56:41 +0300

    feat: add StringExtensions static class for timestamped string formatting

---
+++ src/CloudflareDnsync.Cli/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudflareDnsync.Cli.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToTimestampedString(this string value, string color = "blue", string scope = "CLI")
+    {
+        return $"[[{DateTime.Now:HH:mm:ss} [blue]{scope}[/]]] [bold][white]{value}[/][/]";
+    }
+}
