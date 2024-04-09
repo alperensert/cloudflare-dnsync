c20462d306c60905586a9a454f64d0f5c7da0e0d
alperensert <63921520+alperensert@users.noreply.github.com>
2024-04-09 15:18:32 +0300

    chore: version bump os compability

--- src/CloudflareDnsync.Cli/version_bump.sh
+++ src/CloudflareDnsync.Cli/version_bump.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 VERSION=$1
-sed -i "" "s/<Version>.*<\/Version>/<Version>$VERSION<\/Version>/" ./CloudflareDnsync.Cli.csproj
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" "s/<Version>.*<\/Version>/<Version>$VERSION<\/Version>/" CloudflareDnsync.Cli.csproj
+else
+    sed -i "s/<Version>.*<\/Version>/<Version>$VERSION<\/Version>/" CloudflareDnsync.Cli.csproj
+fi
